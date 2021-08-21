5c95dec67d14d28e9fc8631b3a0cee93fbf80a1c
samgashyeong <samgashyeong@naver.com>
2021-08-21 23:34:46 +0900

    databinding 계산기 구현

--- app/src/main/java/com/example/databijndingpratice/MainActivity.java
+++ app/src/main/java/com/example/databijndingpratice/MainActivity.java
@@ -2,36 +2,36 @@ package com.example.databijndingpratice;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+
+import com.example.databijndingpratice.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity{
 
-
-    EditText firstEdit;
-    EditText secondEdit;
-    TextView sumText;
-    Button addBtn;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
 
-        firstEdit = findViewById(R.id.firstNumEdit);
-        secondEdit = findViewById(R.id.secondNumEdit);
-        sumText = findViewById(R.id.sumText);
-        addBtn = findViewById(R.id.addBtn);
-
-        addBtn.setOnClickListener(v->{
-            int num1 = Integer.parseInt(firstEdit.getText().toString());
-            int num2 = Integer.parseInt(secondEdit.getText().toString());
-            sumText.setText(new StringBuilder().append(num1 + num2).append("").toString());
-        });
+    public static String addIntString(String a, String b){
+        if(a == null ||a.isEmpty() || b == null || b.isEmpty()){
+            return "need two number";
+        }
+        else{
+            return String.valueOf(Integer.parseInt(a) + Integer.parseInt(b));
+        }
     }
 
 
